0ad16b7aadfa85b1c44a7347d3f2e8e181c6d7be
Malwin Niehus <malwin.niehus@gmx.de>
2019-10-07 16:45:46 +0200

    removed unneeded constants

--- cpp/include/constants.h
+++ cpp/include/constants.h
@@ -9,27 +9,6 @@ inline constexpr Number pi()
     return 3.14159265358979323846264338327950288419716939937510;
 }
 
-/// PDG (Particle Data Group) values
-namespace pdg {
-constexpr double pion_decay{92.28}; // in MeV
-constexpr double charged_pion_mass{139.57061}; // in MeV
-constexpr double omega_mass{782.65}; // in MeV
-} // pdg
-
-/// @brief FLAG (Flavour Lattice Averaging Group) values (2019 edition)
-namespace flag {
-/// \f$F_\pi/F\f$ for \f$N_f = 2+1+1\f$.
-constexpr double pion_decay_ratio_four_flavours{1.077};
-
-/// \f$F_\pi/F\f$ for \f$N_f = 2+1\f$.
-constexpr double pion_decay_ratio_three_flavours{1.062};
-
-/// \f$F_\pi/F\f$ for \f$N_f = 2\f$.
-constexpr double pion_decay_ratio_two_flavours{1.073};
-} // flag
-
-constexpr double pion_decay_chiral{
-    pdg::pion_decay/flag::pion_decay_ratio_three_flavours};
 } // constants
 
 

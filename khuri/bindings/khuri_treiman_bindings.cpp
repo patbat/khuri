a8906702fae27b7bdca7c319b5219b1e951bba2f
patbat <niehus@hiskp.uni-bonn.de>
2020-01-29 18:22:05 +0100

    binding for piecewise linear curve

---
+++ khuri/bindings/khuri_treiman_bindings.cpp
@@ -0,0 +1,18 @@
+#include "pybind11/pybind11.h"
+#include "khuri_treiman.h"
+
+namespace py = pybind11;
+using khuri_treiman::Piecewise;
+
+PYBIND11_MODULE(_khuri_khuri_treiman, m) {
+    m.doc() = "Khuri Treiman equations.";
+
+    py::class_<Piecewise> piecewise(m, "Piecewise");
+    piecewise.def(py::init<const std::vector<Complex>&,
+                           const std::vector<Piecewise::Para>&>());
+
+    py::enum_<Piecewise::Para>(piecewise, "Para")
+        .value("linear", Piecewise::Para::linear)
+        .value("quadratic", Piecewise::Para::quadratic)
+        .export_values();
+}

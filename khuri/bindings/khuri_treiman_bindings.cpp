0a5c58df4edc9ea2a75bf9ee02e548b51e2b8c85
patbat <niehus@hiskp.uni-bonn.de>
2020-01-30 16:39:21 +0100

    complete bindings for piecewise linear curve

--- khuri/bindings/khuri_treiman_bindings.cpp
+++ khuri/bindings/khuri_treiman_bindings.cpp
@@ -1,6 +1,9 @@
 #include "khuri_treiman.h"
 
 #include "pybind11/pybind11.h"
+#include "pybind11/complex.h"
+#include "pybind11/numpy.h"
+#include "pybind11/stl.h"
 
 namespace py = pybind11;
 using khuri_treiman::Complex;
@@ -10,8 +13,27 @@ PYBIND11_MODULE(_khuri_khuri_treiman, m) {
     m.doc() = "Khuri Treiman equations.";
 
     py::class_<Piecewise> piecewise(m, "Piecewise");
-    piecewise.def(py::init<const std::vector<Complex>&,
-                           const std::vector<Piecewise::Para>&>());
+    piecewise
+        .def(py::init<const std::vector<Complex>&,
+                      const std::vector<Piecewise::Para>&>())
+        .def("curve_func", py::vectorize(&Piecewise::curve_func),
+             "Evalute the piecewise linear curve at `x`.",
+             py::arg("x"))
+        .def("derivative_func", py::vectorize(&Piecewise::derivative_func),
+             "Evalute the derivative of the piecewise linear curve at `x`.",
+             py::arg("x"))
+        .def("hits", &Piecewise::hits,
+             py::arg("x"))
+        .def("lower", &Piecewise::lower,
+             "Return the parameter value corresponding to the start of the"
+             " curve.")
+        .def("upper", &Piecewise::upper,
+             "Return the parameter value corresponding to the end of the"
+             " curve.")
+        .def("piece_index", py::vectorize(&Piecewise::piece_index),
+                "Return the number of the segment corresponding to the"
+                " parameter value `x`.",
+             py::arg("x"));
 
     py::enum_<Piecewise::Para>(piecewise, "Para")
         .value("linear", Piecewise::Para::linear)
---
+++ khuri/khuri_treiman.py
@@ -0,0 +1,1 @@
+from _khuri_khuri_treiman import *

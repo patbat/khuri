6c69e7e55c225eab6a7e72b137a76c877a7dddad
patbat <niehus@hiskp.uni-bonn.de>
2020-01-30 15:11:37 +0100

    common type aliases

---
+++ cpp/include/type_aliases.h
@@ -0,0 +1,10 @@
+#ifndef TYPE_ALIASES_H
+#define TYPE_ALIASES_H
+
+#include <complex>
+
+namespace type_aliases {
+using Complex = std::complex<double>;
+} // type_aliases
+
+#endif // TYPE_ALIASES_H

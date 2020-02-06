breit wigner partial wave w/ correct normalization

khuri/tests/test_amplitude.py
@@ -1,24 +1,43 @@
+import itertools
+
 import pytest
 import numpy as np
 
-from khuri.madrid import phase, PION_MASS
 import khuri.amplitude as ka
+from khuri.breit_wigner import partial_wave
+from khuri.tests.helpers import connected
 
 
+PION_MASS = 0.14
 CUT = np.linspace(2.0 * PION_MASS + 1, 4.0 * PION_MASS, 50)**2
 
 
-@ka.from_phase(PION_MASS)
 def amplitude(mandelstam_s):
+    return partial_wave(mandelstam_s,
+                        resonance_mass=0.77,
+                        resonance_width=0.15,
+                        angular_momentum=1,
+                        mass=PION_MASS)
+
+
+def phase(mandelstam_s):
+    return np.angle(amplitude(mandelstam_s))
+
+
+@ka.from_phase(PION_MASS)
+def amplitude2(mandelstam_s):
     return phase(mandelstam_s)
 
+
 @ka.from_cot(PION_MASS)
-def amplitude2(mandelstam_s):
+def amplitude3(mandelstam_s):
     return 1.0 / np.tan(phase(mandelstam_s))
 
 
 def test_consistency():
-    assert np.allclose(amplitude(CUT), amplitude2(CUT))
+    amplitudes = (amplitude, amplitude2, amplitude3)
+    for amp1, amp2 in itertools.combinations(amplitudes, 2):
+        assert np.allclose(amp1(CUT), amp2(CUT))
 
 
 class TestSecondSheet:
@@ -28,3 +47,7 @@ class TestSecondSheet:
     def test_identity(self):
         cls = type(self)
         assert np.allclose(cls.first(CUT), amplitude(CUT))
+
+    def test_connected(self):
+        cls = type(self)
+        connected(cls.first, cls.second, CUT)
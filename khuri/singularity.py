a5c41b1e296389ec91483dd0ac68fbcb0ab4cb8c
patbat <niehus@hiskp.uni-bonn.de>
2020-02-28 12:50:54 +0100

    bugfix in c9a14ef

--- khuri/singularity.py
+++ khuri/singularity.py
@@ -3,18 +3,16 @@
 Singularities in Khuri Treiman equations
 ========================================
 """
-from collections.abc import Iterable
-
 import numpy as np
 from numpy.lib.scimath import sqrt  # handles negative reals correctly
+
 
 CUBE_ROOT = (-1.0 + sqrt(3) * 1j) / 2.0
 
 
+@np.vectorize
 def cube_root(value):
-    if isinstance(value, Iterable):
-        return np.array(tuple(float(x)**(1/3) for x in value))
-    return float(value)**(1/3)
+    return complex(value)**(1/3)
 
 
 def cubic_equation(coeff, number):
@@ -23,7 +21,7 @@ def cubic_equation(coeff, number):
     delta1 = 2.0 * c2**3 - 9.0 * c3 * c2 * c1 + 27.0 * c3**2 * c0
     sqr = sqrt(delta1**2 - 4.0 * delta0**3)
     delta = cube_root((delta1 + sqr) / 2.0)
-    if abs(delta) < 1e-20:
+    if np.any(abs(delta) < 1e-20):
         delta = cube_root((delta1 - sqr) / 2.0)
     delta *= CUBE_ROOT**number
     return -1.0 * (c2 + delta + delta0 / delta) / 3.0 / c3
@@ -39,12 +37,12 @@ def coefficient_2(cos2, decay_mass_2, mass_2):
 
 
 def coefficient_1(cos2, decay_mass_2, mass_2):
-    return ((cos2 * (decay_mass_2 + 3.0 * mass_2))**2
+    return (cos2 * (decay_mass_2 + 3.0 * mass_2)**2
             - (decay_mass_2 - 5.0 * mass_2)**2)
 
 
 def coefficient_0(cos2, decay_mass_2, mass_2):
-    return - (2.0 * cos2 * mass_2 * (decay_mass_2 - mass_2))**2
+    return - 4.0 * cos2 * mass_2 * (decay_mass_2 - mass_2)**2
 
 
 def coefficients(cos2, decay_mass_2, mass_2):
@@ -57,3 +55,11 @@ def coefficients(cos2, decay_mass_2, mass_2):
 def singularity(cos2, decay_mass_2, mass_2, number):
     coeff = coefficients(cos2, decay_mass_2, mass_2)
     return cubic_equation(coeff, number)
+
+
+def singularity_curve(decay_mass_2, mass_2, number, points=200, eps=1e-6):
+    # The bounds needs to be slightly adapted, for otherwise the
+    # general solution to the cubic equation becomes singular (at cos2 == 0, 1
+    # the cubic equation reduces to a quadratic one).
+    cos2 = np.linspace(0 + eps, 1 - eps, points)
+    return singularity(cos2, decay_mass_2, mass_2, number)

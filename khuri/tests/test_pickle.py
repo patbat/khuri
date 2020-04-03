33111e8cf3cc7213e833ea65c951c61c0cee2768
Malwin Niehus <malwin.niehus@gmx.de>
2020-04-03 17:43:31 +0200

    test: is omnes pickleable?

--- khuri/tests/test_pickle.py
+++ khuri/tests/test_pickle.py
@@ -1,6 +1,16 @@
 import contextlib
+import pickle
 import os
 import shutil
+
+import pytest
+
+from khuri.tests.test_omnes import OMNES_LIST
+
+
+@pytest.fixture
+def omnes_function():
+    return OMNES_LIST[0]
 
 
 @contextlib.contextmanager
@@ -13,6 +23,11 @@ def environment():
         shutil.rmtree(directory)
 
 
-def test():
+def test_omnes(omnes_function):
     with environment() as env:
-        print(env)
+        path = os.path.join(env, 'omnes.pickle')
+        with open(path, 'wb') as pickle_file:
+            pickle.dump(omnes_function, pickle_file)
+        with open(path, 'rb') as pickle_file:
+            func = pickle.load(pickle_file)
+            assert func(0.0) == pytest.approx(0.0)
